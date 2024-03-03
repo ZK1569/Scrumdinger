9c9850681285729e562da02e2457f9e96abacab3
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-03-03 18:33:59 +0100

    refacto meeting timer

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,40 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Cristian Ursu on 03/03/2024.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+    
+    private var currentSpeaker: String {
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
+    }
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text("is speaking")
+                }
+                .accessibilityElement(children: .combine)
+                .foregroundStyle(theme.accentColor)
+            }
+    }
+}
+
+struct MeetingTimerView_Preview: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    }
+    static var previews: some View {
+        MeetingTimerView(speakers: speakers, theme: .yellow)
+    }
+}
